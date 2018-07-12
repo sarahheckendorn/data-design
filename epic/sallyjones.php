9afdc91b2238740b072ce0ad92c6ec2702393738
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-12 13:20:27 -0600

    Changed image place and eliminated duplicate of name

---
+++ epic/sallyjones.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF8">
+		<title>Sally Jones</title>
+	</head>
+	<body>
+		<header>
+			<h1>Sally Jones<h1/>
+				<img src="sallyjones.jpg" alt="SallyJones.jpg">
+		</header>
+		<div>
+			<ul>
+				<li>Gender: Female</li>
+				<li>Age: 25</li>
+				<li>Single</li>
+				<li>Tech Comfortability: Mid-level</li>
+			</ul>
+		</div>
+		<div>
+		<p>Sally enjoys playing her guitar and singing at the top of her voice.<br>
+			She loves learning new songs and is constantly looking up sites that can offer chords, tabs,<br>
+			and lyrics to her favorite songs or new jams.</p>
+		<p>Sally likes to find her music quickly and efficiently so that it doesn't take from playing time.</p>
+		<h3>Tech Used:</h3>
+		<p>Dell Inspiron 17R - 17.3" - Core i3 4010U</p>
+		<p>Google Pixel 2 Android 8.0.0, Oreo</p>
+		</div>
+	</body>
+</html>
