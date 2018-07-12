ef1593b8f82b124ed943c4b8711f2410b00c84b2
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-12 14:31:56 -0600

    updated with the rest

---
+++ epic/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF8">
+			<title>Data Design</title>
+		</head>
+	<body>
+		<header>
+			<h1>Navigation for Data Design</h1>
+			<nav>
+				<ul>
+					<li><a href="./conceptual-model.php" alt="link to conceptual model"/a>Conceptual Model</li>
+					<li><a href="./sallyjones.php" alt="link to persona"/a>Sally Jones</li>
+					<li><a href="./user-case.php" alt="link to user case"/a> User Case</li>
+				</ul>
+			</nav>
+		</header>
+	</body>
+</html>
