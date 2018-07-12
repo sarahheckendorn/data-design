ceba0dac6732eb73bc8c6bbaca51c1b9d33ea101
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-12 14:33:44 -0600

    Removed features and frustrations and changed user case

---
+++ epic/user-case.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF8">
+		<title>Data Design</title>
+	</head>
+	<body>
+		<div>
+			<h2>Use Case</h2>
+			<p>Sally needs to get a song with tabs.</p>
+		</div>
+		<div>
+			<h2>Interaction Flow</h2>
+			<ul>
+				<li>Sally opens her browser and enters URL</li>
+				<li>Server takes information and returns site</li>
+				<li>Sally clicks on search box and enters song name</li>
+				<li>Site pulls lists of songs most closely related to search</li>
+				<li>Sally clicks on song title she wants</li>
+				<li>Site opens song page</li>
+				<li>Sally plays along with song</li>
+			</ul>
+		</div>
+	</body>
+</html
