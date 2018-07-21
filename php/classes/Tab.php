bd0a5c934b2be36d07e29d22a097cc24bda2e206
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-21 14:40:11 -0600

    put in insert, delete, update, and get

--- php/classes/Tab.php
+++ php/classes/Tab.php
@@ -88,4 +88,83 @@ class Tab {
 		}
 	$this->tabImageUrl = $newTabImageUrl;
 	}
+	/**
+	 * inserts this tabId into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo): void {
+		// create query template
+		$query = "INSERT INTO tab(tabId, tabImageUrl) VALUES (:tabId, :tabImageUrl)";
+		$statement = $pdo->prepare($query);
+		$parameters = ["tabId" => $this->tabId->getBytes(), "tabImageUrl" => $this->tabImageUrl];
+		$statement->execute($parameters);
+	}
+	/**
+	 * deletes this tabId from mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo): void {
+		// create query template
+		$query = "DELETE FROM tab WHERE tabId = :tabId";
+		$statement = $pdo->prepare($query);
+		//bind the member variables to the place holders in the template
+		$parameters = ["tabId" => $this->tabId->getBytes()];
+		$statement->execute($parameters);
+	}
+	/**
+	 * updates this Tab from mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 **/
+	public function update(\PDO $pdo): void {
+		// create query template
+		$query = "UPDATE tab SET tabImageUrl = :tabImageUrl WHERE tabId = :tabId";
+		$statement = $pdo->prepare($query);
+		// bind the member variables to the place holders in the template
+		$parameters = ["tabId" => $this->tabId->getBytes(), "tabImageUrl" => $this->tabImageUrl];
+		$statement->execute($parameters);
+	}
+	/**
+	 * gets the Tab by tab id
+	 *
+	 * @param \PDO $pdo $pdo PDO connection object
+	 * @param string $tabId Tab Id to search for
+	 * @return Tab|null Tab or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when a variable are not the correct data type
+	 **/
+	public static function getTabByTabId(\PDO $pdo, string $tabId):?Tab {
+		// sanitize the tab id before searching
+		try {
+			$tabId = self::validateUuid($tabId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		// create query template
+		$query = "SELECT tabId, tabImageUrl FROM tab WHERE tabId = :tabId";
+		$statement = $pdo->prepare($query);
+		// bind the tab id to the place holder in the template
+		$parameters = ["tabId" => $tabId->getBytes()];
+		$statement->execute($parameters);
+		// grab the Tab from mySQL
+		try {
+			$tab = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$tab = new Tab($row["tabId"], $row["tabImageUrl"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($tab);
+	}
 }
