3f110cd7636a51b275fad1fcdd863f551ad2b744
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-23 17:04:54 -0600

    fixed path

--- php/classes/SongTab.php
+++ php/classes/SongTab.php
@@ -3,7 +3,7 @@
 namespace Sarahheckendorn\DataDesign;
 
 require_once("autoload.php");
-require_once(dirname(__DIR__, 2) . "../vendor/autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 
